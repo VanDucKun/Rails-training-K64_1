10eea6cd651332e2e049eea9893ecf48ddb506a8
VanDucKun <havanducdeptrai@gmail.com>
2023-01-31 09:21:57 +0000

    Add review book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,6 +11,10 @@ class BooksController < ApplicationController
   def show
   end
 
+  def show
+      @book_review = BookReview.new
+  end
+
   # GET /books/new
   def new
     @book = Book.new
@@ -66,6 +70,6 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      params.require(:book).permit(:title, :number_of_pages, :image, :detail)
+      params.require(:book).permit(:title, :number_of_pages, :detail, :image)
     end
 end
